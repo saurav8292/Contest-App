2e64f72ee8862943bed0aabf0106f56da748da0f
saurav <saurav8292@gmail.com>
2021-06-08 18:39:50 +0530

    updatePassword variables issue solved

--- src/component/Profile/ChangePassword/ChangePassword.js
+++ src/component/Profile/ChangePassword/ChangePassword.js
@@ -28,12 +28,39 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ChangePassword() {
+  const classes1 = useStyles();
+  const [values1, setValues1] = React.useState({
+    password1: '',
+    showPassword1: false,
+  });
+  const handleChange1 = (prop) => (event) => {
+    setValues1({ ...values1, [prop]: event.target.value });
+  };
+  const handleClickShowPassword1 = () => {
+    setValues1({ ...values1, showPassword1: !values1.showPassword1 });
+  };
+  const handleMouseDownPassword1 = (event) => {
+    event.preventDefault();
+  };
+
+  const classes2 = useStyles();
+  const [values2, setValues2] = React.useState({
+    password2: '',
+    showPassword2: false,
+  });
+  const handleChange2 = (prop) => (event) => {
+    setValues2({ ...values2, [prop]: event.target.value });
+  };
+  const handleClickShowPassword2 = () => {
+    setValues2({ ...values2, showPassword2: !values2.showPassword2 });
+  };
+  const handleMouseDownPassword2 = (event) => {
+    event.preventDefault();
+  };
+
   const classes = useStyles();
   const [values, setValues] = React.useState({
-    amount: '',
     password: '',
-    weight: '',
-    weightRange: '',
     showPassword: false,
   });
 
@@ -54,22 +81,22 @@ export default function ChangePassword() {
       <div className="container">
 
       <div className="form-row frms">
-        <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Current Password</InputLabel>
+        <FormControl className={clsx(classes1.margin, classes1.textField)} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password1">Current Password</InputLabel>
           <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
+            id="outlined-adornment-password1"
+            type={values1.showPassword1 ? 'text' : 'password'}
+            value={values1.password1}
+            onChange={handleChange1('password1')}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
+                  aria-label="toggle password1 visibility"
+                  onClick={handleClickShowPassword1}
+                  onMouseDown={handleMouseDownPassword1}
                   edge="end"
                 >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  {values1.showPassword1 ? <Visibility /> : <VisibilityOff />}
                 </IconButton>
               </InputAdornment>
             }
@@ -79,22 +106,22 @@ export default function ChangePassword() {
         </div>
           
         <div className="form-row frms">
-        <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password">New Password</InputLabel>
+        <FormControl className={clsx(classes2.margin, classes2.textField)} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password2">New Password</InputLabel>
         <OutlinedInput
-          id="outlined-adornment-password"
-          type={values.showPassword ? 'text' : 'password'}
-          value={values.password}
-          onChange={handleChange('password')}
+          id="outlined-adornment-password2"
+          type={values2.showPassword2 ? 'text' : 'password'}
+          value={values2.password2}
+          onChange={handleChange2('password2')}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
+                aria-label="toggle password2 visibility"
+                onClick={handleClickShowPassword2}
+                onMouseDown={handleMouseDownPassword2}
                 edge="end"
               >
-                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                {values2.showPassword2 ? <Visibility /> : <VisibilityOff />}
               </IconButton>
             </InputAdornment>
           }
@@ -129,7 +156,7 @@ export default function ChangePassword() {
     </div>
       
     <div className="form-row submit">
-    <Button variant="contained" color="primary">Change Password</Button>
+    <Button variant="contained" style={{backgroundColor:"#CCCCFF"}}>Change Password</Button>
     </div>
 
       </div>
