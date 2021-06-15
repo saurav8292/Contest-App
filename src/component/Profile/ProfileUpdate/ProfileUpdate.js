8bd51dadc9c52fcf2245d4d27edb739db2aba06d
saurav <saurav8292@gmail.com>
2021-06-15 14:14:22 +0530

    profileUpdate Done

--- src/component/Profile/ProfileUpdate/ProfileUpdate.js
+++ src/component/Profile/ProfileUpdate/ProfileUpdate.js
@@ -1,24 +1,41 @@
 import React, { useEffect, useState } from "react";
-import DatePicker from "react-datepicker";
 import "./ProfileUpdate.css";
-import "react-datepicker/dist/react-datepicker.css";
 import Button from "@material-ui/core/Button";
 import { db } from "../../Configure/Fire";
 import { useAuth } from "../../../Context/AuthContext";
+import { useHistory } from 'react-router';
+
 
 const ProfileUpdate = () => {
-  const [dob, setDob] = useState("");
   const { currentUser } = useAuth();
-  const [nameFromDb, setnameFromDb] = useState("");
+  const [uname,setname]=useState("");
+  const [ucity,setcity]=useState("");
+  const [ustate,setstate]=useState("")
+  const [ucountry,setcountry]=useState("");
+  const [message, setMessage] = useState("");
+
+  const history = useHistory();
+
   console.log(currentUser.uid);
-  const uploadTODB = () => {
+
+  const uploadTODB = async (e) => {
+    e.preventDefault();
     db.collection("users").doc(currentUser.uid).set({
-      name: "Kishan",
+      name:uname,
+      city:ucity,
+      state:ustate,
+      country:ucountry,
     });
+    setMessage("Profile Updated!");
+    setTimeout(()=>{
+      history.push('/Profile');
+    },1000);
   };
   const getDataFromDb = async () => {
     const snap = await db.collection("users").doc(currentUser.uid).get();
     const name = snap.data()["name"];
+    const city =snap.data()["city"];
+    console.log(city);
     console.log(name);
   };
   useEffect(() => {
@@ -27,6 +44,7 @@ const ProfileUpdate = () => {
 
   return (
     <div className="container" style={{ marginTop: "70px" }}>
+    <form>
       <div className="form-row">
         <div className="col-md-4 mb-3">
           <label
@@ -43,28 +61,13 @@ const ProfileUpdate = () => {
             type="text"
             className="form-control"
             name="name"
+            value={uname}
             placeholder="Name"
+            onChange={(e)=>setname(e.target.value)}
           />
         </div>
 
-        <div className="col-md-4 mb-3">
-          <label
-            style={{
-              float: "left",
-              fontSize: "1.2em",
-              fontWeight: "600px",
-              marginLeft: "2px",
-            }}
-          >
-            Email
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            name="email"
-            placeholder="Email"
-          />
-        </div>
+       
 
         <div className="col-md-4 mb-3">
           <label
@@ -82,6 +85,8 @@ const ProfileUpdate = () => {
             className="form-control"
             name="city"
             placeholder="City"
+            value={ucity}
+            onChange={(e)=>setcity(e.target.value)}
           />
         </div>
 
@@ -101,6 +106,8 @@ const ProfileUpdate = () => {
             className="form-control"
             name="state"
             placeholder="State"
+            value={ustate}
+            onChange={(e)=>setstate(e.target.value)}
           />
         </div>
 
@@ -120,15 +127,8 @@ const ProfileUpdate = () => {
             className="form-control"
             name="country"
             placeholder="Country"
-          />
-        </div>
-
-        <div className="dob">
-          <label className="Dob">Date Of Birth :-</label>
-          <DatePicker
-            selected={dob}
-            onChange={(date) => setDob(date)}
-            name="dob"
+            value={ucountry}
+            onChange={(e)=>setcountry(e.target.value)}
           />
         </div>
       </div>
@@ -139,11 +139,10 @@ const ProfileUpdate = () => {
         color="primary"
         style={{ marginTop: "10px" }}
       >
-        Submit Dummy Data
-      </Button>
-      <Button variant="contained" color="primary" style={{ marginTop: "10px" }}>
         Submit
       </Button>
+      </form>
+      <h4>{message}</h4>
     </div>
   );
 };
